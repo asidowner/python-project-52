e2d460e8f89961e95853fd01964c8c7175812364
Anton Sidorenkov <asid.owner@gmail.com>
2022-12-22 22:36:33 +0400

    Step #8.3 add on method user/delete check about exists tasks

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -4,12 +4,14 @@ from django.views import generic
 from django.shortcuts import redirect
 from django.contrib import messages
 
+from django.db.models import Q
 from django.contrib.auth import models as auth_models
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
 from django.utils.translation import gettext_lazy as _
 
+from task_manager.tasks.models import Task
 from task_manager.users.forms import UserCreateForm
 
 _CREATE_USER_SUCCESS_MESSAGE = _('User is successfully registered')
@@ -17,8 +19,10 @@ _UPDATE_USER_SUCCESS_MESSAGE = _('User is successfully changed')
 _UPDATE_USER_ERROR_MESSAGE = _("You don't have permission"
                                " to change another user.")
 _DELETE_USER_SUCCESS_MESSAGE = _('User is successfully deleted')
-_DELETE_USER_ERROR_MESSAGE = _("You don't have permission"
-                               " to delete another user.")
+_DELETE_USER_PERMISSION_ERROR_MESSAGE = _("You don't have permission"
+                                          " to delete another user.")
+_DELETE_USER_USED_ERROR_MESSAGE = _('Unable to delete a user'
+                                    ' because he is being used')
 
 
 class UserListView(generic.ListView):
@@ -58,14 +62,26 @@ class UserDeleteView(LoginRequiredMixin,
     model = auth_models.User
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users:list')
+    error_url = reverse_lazy('users:list')
     success_message = _DELETE_USER_SUCCESS_MESSAGE
+
+    def post(self, request, *args, **kwargs):
+        if self._is_have_task():
+            messages.error(self.request, _DELETE_USER_USED_ERROR_MESSAGE)
+            return redirect(self.error_url)
+        return super().post(request, *args, **kwargs)
+
+    def _is_have_task(self) -> bool:
+        return Task.objects.filter(
+            Q(author=self.request.user) | Q(executor=self.request.user)
+        ).exists()
 
     def test_func(self):
         return self._is_self_user()
 
-    def _is_self_user(self):
+    def _is_self_user(self) -> bool:
         return self.kwargs['pk'] == self.request.user.id
 
     def handle_no_permission(self):
-        messages.error(self.request, _DELETE_USER_ERROR_MESSAGE)
-        return redirect(reverse_lazy('users:list'))
+        messages.error(self.request, _DELETE_USER_PERMISSION_ERROR_MESSAGE)
+        return redirect(self.error_url)
